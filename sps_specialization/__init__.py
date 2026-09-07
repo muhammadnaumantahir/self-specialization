58a5240765297de6197ab2809c65092efdf7c440
Muhammad Nauman Tahir <muhammadnauman.tahir0@gmail.com>
2026-09-07 13:54:07 +0500

    refactor: expose deterministic specialization components and remove AI client

--- sps_specialization/__init__.py
+++ sps_specialization/__init__.py
@@ -2,8 +2,15 @@ from .capability import Capability, Event
 from .dispatcher import CapabilityDispatcher
 from .evolution import EvolutionEngine
 from .handler import CapabilityHandler
-from .ollama_client import OllamaClient
 from .registry import CapabilityRegistry
+from .specialization import SpecializationEngine
+from .type_specialization import TypeSpecializationTransformer
+from .type_specialization_rules import (
+    TYPE_SPECIALIZATION_RULES,
+    TypeSpecializationRule,
+    get_type_specialization_rule,
+    is_type_specialization_allowed,
+)
 from .verifier import Verifier
 
 __all__ = [
@@ -12,7 +19,12 @@ __all__ = [
     "CapabilityHandler",
     "CapabilityDispatcher",
     "EvolutionEngine",
-    "OllamaClient",
+    "SpecializationEngine",
+    "TypeSpecializationTransformer",
+    "TypeSpecializationRule",
+    "TYPE_SPECIALIZATION_RULES",
+    "get_type_specialization_rule",
+    "is_type_specialization_allowed",
     "CapabilityRegistry",
     "Verifier",
 ]
